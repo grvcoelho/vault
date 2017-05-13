0e42016f11f75a4f8a07c99773b9ae7eb76a3c5a
Guilherme Coelho <guilhermervcoelho@gmail.com>
2017-05-13 17:27:20 -0300

    infra: add groups and policies
    Add policies to access KMS and Database features. Also remove iam_users
    and use iam_groups instead. The created groups are:
    
    - vault_readers
    - vault_writers

--- infra/main.tf
+++ infra/main.tf
@@ -10,12 +10,12 @@ provider "aws" {
 # Users
 # ============================
 
-resource "aws_iam_user" "vault_client" {
-  name = "vault_client"
+resource "aws_iam_group" "vault_readers" {
+  name = "vault_readers"
 }
 
-resource "aws_iam_user" "vault_admin" {
-  name = "vault_admin"
+resource "aws_iam_group" "vault_writers" {
+  name = "vault_writers"
 }
 
 # ============================
@@ -53,5 +53,82 @@ resource "aws_kms_alias" "vault_master_key_alias" {
   target_key_id = "${aws_kms_key.vault_master_key.key_id}"
 }
 
+# ============================
+# Access Policies
+# ============================
 
+data "aws_iam_policy_document" "vault_read_policy_document" {
+  statement {
+    effect = "Allow"
 
+    actions = [
+      "kms:Decrypt"
+    ]
+
+    resources = [
+      "${aws_kms_key.vault_master_key.arn}"
+    ]
+  }
+
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "dynamodb:GetItem",
+      "dynamodb:Query",
+      "dynamodb:Scan"
+    ]
+
+    resources = [
+      "${aws_dynamodb_table.vault_database.arn}"
+    ]
+  }
+}
+
+resource "aws_iam_policy" "vault_read_policy" {
+  name = "vault_read_policy"
+  policy = "${data.aws_iam_policy_document.vault_read_policy_document.json}"
+}
+
+resource "aws_iam_policy_attachment" "vault_read_policy_attachment" {
+  name = "vault_read_policy_attachment"
+  groups = ["${aws_iam_group.vault_readers.name}"]
+  policy_arn = "${aws_iam_policy.vault_read_policy.arn}"
+}
+
+data "aws_iam_policy_document" "vault_write_policy_document" {
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "kms:GenerateDataKey"
+    ]
+
+    resources = [
+      "${aws_kms_key.vault_master_key.arn}"
+    ]
+  }
+
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "dynamodb:PutItem"
+    ]
+
+    resources = [
+      "${aws_dynamodb_table.vault_database.arn}"
+    ]
+  }
+}
+
+resource "aws_iam_policy" "vault_write_policy" {
+  name = "vault_write_policy"
+  policy = "${data.aws_iam_policy_document.vault_write_policy_document.json}"
+}
+
+resource "aws_iam_policy_attachment" "vault_write_policy_attachment" {
+  name = "vault_write_policy_attachment"
+  groups = ["${aws_iam_group.vault_writers.name}"]
+  policy_arn = "${aws_iam_policy.vault_write_policy.arn}"
+}
